708a6e2f6dfc42e0c2b67e7600216395806d81a0
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-12 18:03:50 +0530

    practice 3

---
+++ chapter1(pr)/p3.py
@@ -0,0 +1,11 @@
+# module name hai pyttsx3
+
+# import pyttsx3
+# pyttsx3.speak("Deepanshu kumar")
+
+# or
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hey I am good")
+engine.runAndWait()
