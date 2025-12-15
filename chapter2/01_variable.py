fcea53f25a2121af07fec4b963386bc288a2c041
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-15 22:01:44 +0530

    variables

---
+++ chapter2/01_variable.py
@@ -0,0 +1,26 @@
+z="52"
+a="deepanshu"
+b=7
+c=3.9
+d=True              # isme T capital hota hai
+print("  is ",c)
+ 
+
+e=7/3
+print(e)   
+print(not (e))     
+
+
+f=b>c
+print(f)
+
+g=False or True
+print(g)
+
+
+print(type (a))
+
+print(float(z))
+print(str(b))
+print(bool(b))
+print(int(c))
