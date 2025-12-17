9df538891c5d1ee13c57e6ea6ff65d093b773de2
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-17 19:41:46 +0530

    string

---
+++ chapter3/0_string.py
@@ -0,0 +1,17 @@
+a='deepanshu kumar'
+b="kumar"
+c="'deep'"
+
+print(a[0])
+print(a[0:4])
+print(a[1:7]) 
+print(len(a))
+print(a[-7:-1])
+print(a[0:9:1])
+print(a[0:9:2])
+
+
+print(a.capitalize())#sirf D ko capital mei likha hai
+print(a.replace("a","aa"))
+
+
