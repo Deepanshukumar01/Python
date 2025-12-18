67a164f7b61ac708ab28e74a610a6e97a92fb23a
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-18 22:27:02 +0530

    p1

---
+++ chapter2(pr)/p1.py
@@ -0,0 +1,9 @@
+a=float(input("enter the a:"))
+b=float(input("enter the b:"))
+
+c=input("enter the c:")
+
+print("sum of a + b is :",a+b)                      # 1
+print("remander when a divided by b :",a%b)         #2
+
+print(type(c))         #3.  by default ye str mei save krti hai
