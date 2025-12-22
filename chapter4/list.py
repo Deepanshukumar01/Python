6dbba0bd298e859b67d13444999ea237772a287c
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-22 21:51:42 +0530

    list

---
+++ chapter4/list.py
@@ -0,0 +1,17 @@
+about=["Deepanshu","kumar",52,   19.5,True]
+
+print(about)
+
+print(len(about))
+
+about[3]=19
+print(about)
+
+
+
+about.append("8439656")#and " ki jagh ' hi hota hai
+print(about)
+
+about.insert(1,"kkk")    #mean 1 ke baad add ho jayega
+print(about)
+
