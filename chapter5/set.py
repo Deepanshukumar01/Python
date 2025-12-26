d056b16995e29b5695df6a8665f3b216bf4664e4
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2025-12-26 23:50:27 +0530

    set

---
+++ chapter5/set.py
@@ -0,0 +1,9 @@
+a={} #ye to dic type hua.   to none set kaise bnega
+print(type(a))
+
+b=set()
+print(type(b)) #aisa hoga none set print
+
+
+a={4,3,2,1,"r",5,6,7,8,9}     #order isme mantain nhi hota
+print(a)
