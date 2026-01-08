983e1282139e74bd156a5a93565a2fb077292d05
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-01-08 08:41:18 +0530

    practice

---
+++ chapter5(pr)/p2.py
@@ -0,0 +1,15 @@
+numbers=set()
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=int(input("enter the number:"))
+d=int(input("enter the number:"))
+e=int(input("enter the number:"))
+f=int(input("enter the number:"))
+g=int(input("enter the number:"))
+h=int(input("enter the number:"))
+numbers.add(a)
+numbers.add(b)
+numbers.add(c)
+
+
+print(numbers)
