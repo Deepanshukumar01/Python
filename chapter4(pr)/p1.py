7ff87f53dec0f9ea3d4952171bd2c9bc55929377
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-01-18 23:09:47 +0530

    practice

---
+++ chapter4(pr)/p1.py
@@ -0,0 +1,18 @@
+fruit=[]
+a=input("enter fruit name 1")
+fruit.append(a)
+b=input("enter fruit name 2")
+fruit.append(b)
+c=input("enter fruit name 3")
+fruit.append(c)
+d=input("enter fruit name 4")
+fruit.append(d)
+e=input("enter fruit name 5")
+fruit.append(e)
+f=input("enter fruit name 6")
+fruit.append(f)
+g=input("enter fruit name 7")
+fruit.append(g)
+
+
+print(fruit)
