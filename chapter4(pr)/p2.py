7c36c646552fcf2535c5f2bcfb183ff4e5ab5705
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-01-19 23:08:24 +0530

    practice

---
+++ chapter4(pr)/p2.py
@@ -0,0 +1,19 @@
+marks=[]
+a=int(input("enter marks 1"))
+marks.append(a)
+b=int(input("enter marks 2"))
+marks.append(b)
+c=int(input("enter marks 3"))
+marks.append(c)
+d=int(input("enter marks 4"))
+marks.append(d)
+e=int(input("enter marks 5"))
+marks.append(e)
+f=int(input("enter marks 6"))
+marks.append(f)
+g=int(input("enter marks 7"))
+marks.append(g)
+
+h=marks.sort()
+print(marks)     #ye shi answer is liye nhi de rha because isme bydefault string mei save hoti hai chej pr ab hoga shi
+
