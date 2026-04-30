873d455df6b49a194e494829a71780ae7786d692
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-04-30 23:27:27 +0530

    practce

---
+++ chapter5(pr)/p6.py
@@ -0,0 +1,20 @@
+s={ 
+   
+}
+name=input("enter the name")
+lang=input("enter the fav language")
+s.update({name:lang})
+
+name=input("enter the name")
+lang=input("enter the fav language")
+s.update({name:lang})
+
+name=input("enter the name")
+lang=input("enter the fav language")
+s.update({name:lang})
+
+name=input("enter the name")
+lang=input("enter the fav language")
+s.update({name:lang})
+
+print(s)
