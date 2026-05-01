3981047d5548678242187888fd9d24d2a2dc9680
Deepanshu Kumar <deepanshukumar8705@gmail.com>
2026-05-01 23:36:05 +0530

    practice

---
+++ chapter4(pr)/p4.py
@@ -0,0 +1,12 @@
+# a=int(input("enter number 1"))
+# b=int(input("enter number 2"))
+# c=int(input("enter number 3"))
+# d=int(input("enter number 4"))
+
+# sum=a+b+c+d
+# print(f"sum is {sum}")
+
+
+a=[4,6,7,3]
+
+print(sum(a))
